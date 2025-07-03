94a500e85f22d3e1a3bae9e4dbe3ba5c9bc40b4b
Ikhwan Dian <muhammadikhwandk@gmail.com>
2025-07-03 18:50:48 +0700

    base url

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Loading from './components/Loading';
 import Maps from './pages/Maps';
 import Main from './pages/Main';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 
-
 function App() {
   return (
-    <>
+    <Router> 
       <Loading />
       <div>
         <Routes>
-          <Route path="/" element={<Main />}></Route>
+          <Route path="/" element={<Main />} />
           <Route path="/maps" element={<Maps />} />
         </Routes>
       </div>
-    </>
+    </Router>  
   );
 }
 
