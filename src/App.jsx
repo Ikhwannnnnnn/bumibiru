c551b402491d98a3aa5acd05edad30cd87be2672
Ikhwan Dian <muhammadikhwandk@gmail.com>
2025-07-03 19:24:17 +0700

    fix patch and redirects 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Loading from './components/Loading';
 import Maps from './pages/Maps';
 import Main from './pages/Main';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 
+
 function App() {
   return (
-    <Router> 
+    <>
       <Loading />
       <div>
         <Routes>
-          <Route path="/" element={<Main />} />
+          <Route path="/" element={<Main />}></Route>
           <Route path="/maps" element={<Maps />} />
         </Routes>
       </div>
-    </Router>  
+    </>
   );
 }
 
